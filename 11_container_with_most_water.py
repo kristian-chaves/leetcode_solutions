bcc5e825a6aa05fe08af13e7b8e2b27724682cd4
kristian:) <kristianchaves42@gmail.com>
2024-01-01 12:18:01 -0500

    renamed

---
+++ 11_container_with_most_water.py
@@ -0,0 +1,12 @@
+def maxArea(height: list):
+    max = 0
+    # 1 poter
+    p1 = 0
+    p2 = len(height) - 1
+
+
+    return max
+
+list1 = [1,8,6,2,5,4,8,3,7] 
+print("Ans:")
+print(maxArea(list1))
