96a4c8d97d3610e531771a2646f2dc451ed9525c
kristian:) <kristianchaves42@gmail.com>
2024-04-21 03:12:41 -0400

    Update 14_Longest_Common_Prefix.py

--- 14_Longest_Common_Prefix.py
+++ 14_Longest_Common_Prefix.py
@@ -1,13 +1,11 @@
 def LongestCommon(strs): 
-    print(strs)
-    print(*strs)
-    print(tuple(zip(*strs)))
     finalStr = "" 
-    for x in zip(*strs):
-        if len(set(x)) == 1:
-            finalStr += x[0]
-        else:
-            return finalStr
+    for x in range(len(strs[0])):
+        for s in strs:
+            if len(s) == x or s[x] != strs[0][x]:
+                return finalStr
+        finalStr += strs[0][x]
+    return finalStr
 
 
 print(LongestCommon(["flower","flow","flight"]))
