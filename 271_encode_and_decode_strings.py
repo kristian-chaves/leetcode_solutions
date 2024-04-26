539108cb3de7439a04c1770eb06a092004d4f3fe
kristian:) <kristianchaves42@gmail.com>
2024-04-26 01:20:30 -0400

    Update 271_encode_and_decode_strings.py

--- 271_encode_and_decode_strings.py
+++ 271_encode_and_decode_strings.py
@@ -1,14 +1,23 @@
 def encode(strs):
-    str = ''
+    string = ''
+
     for x in strs:
-        str = str + x + ","
-    str = str[:-1]
-    return str
+        string += str(len(x)) + ":" + x
+    return string
 
 def decode(s):
-    return s.split(",")
+    strs, p = [], 0
+    while p < len(s):
+        j = p
+        while s[j] != ":":
+            j+=1
+        length = int(s[p:j])
+        strs.append(s[j + 1 : j + 1 + length])
+        p = j + 1 + length
+    return strs
 
 strings = ["neet","code","love","you"]
-
+strings1 = []
+strings2 = [""]
 print(encode(strings))
 print(decode(encode(strings)))
