c0170e3614c23f452c2d6fc9ca1825e9be2b2271
kristian:) <kristianchaves42@gmail.com>
2024-04-26 00:45:03 -0400

    Update 238_product_of_array_except_self.py

--- 238_product_of_array_except_self.py
+++ 238_product_of_array_except_self.py
@@ -1,11 +1,19 @@
 def productOfArray(nums):
-    l2 = []
-    for x in range(0, len(nums)):
-        l2.append(1)
-        for y in range(len(nums)):
-            if(x != y):
-                l2[x] *= nums[y]
-    return l2
+    forward = [1] * len(nums)
+    backward = [1] * len(nums)
+    final = [1] * len(nums)
+
+    forward[0] = nums[0]
+    backward[len(nums) -1] = nums[len(nums) -1] 
+    for x in range(1, len(nums)):
+        forward[x] = nums[x] * forward[x-1]
+        backward[len(nums) - 1 - x] = nums[len(nums) - x - 1] * backward[len(nums)-x]
+
+    final[0] = backward[1]
+    for x in range(1, len(nums) - 1):
+        final[x] = forward[x - 1] * backward[x +1]
+    final[len(nums) - 1] = forward[len(nums) - 2]
+    return final
 
 nums = [1,2,3,4]
 print(productOfArray(nums))
