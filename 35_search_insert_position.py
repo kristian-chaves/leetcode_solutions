43a4fb2176cf40ce1278decd55072ca2dbf22a9f
kristian:) <kristianchaves42@gmail.com>
2024-04-29 17:46:51 -0400

    Create 35_search_insert_position.py

---
+++ 35_search_insert_position.py
@@ -0,0 +1,18 @@
+import math
+
+def search_insert_position(nums, target):
+    lower = 0
+    upper = len(nums)
+    while lower <= upper:
+        dest = (lower + upper) // 2
+        if(target == nums[dest]):
+            return dest
+        elif(target > nums[dest]):
+            lower = dest + 1
+        else : #(target > nums[dest])
+            upper = dest - 1
+    return lower
+
+nums = [1,3,5,6]
+target = 7
+print(search_insert_position(nums, target))
