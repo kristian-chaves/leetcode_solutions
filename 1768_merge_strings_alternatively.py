4cc7683dcb8a6ce634f7b36b2e1fc63912bdffa1
kristian:) <kristianchaves42@gmail.com>
2024-05-05 21:54:11 -0400

    Update 1768_merge_strings_alternatively.py

--- 1768_merge_strings_alternatively.py
+++ 1768_merge_strings_alternatively.py
@@ -3,9 +3,9 @@ def merge_string(s1, s2):
     s3 = ""
     turn = 1
     index = 0
-    while s1 or s2:
+    while len(s1) < index and len(s2) < index:
         if turn == 1:
-            s3.append(s1.pop())
+            s3.append()
     return
 
 s1 = "abc"
