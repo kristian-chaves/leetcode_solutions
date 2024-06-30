d581e6d3f6d9e1d3deeadec7e5d7e5356fbaa592
kristian:) <kristianchaves42@gmail.com>
2024-06-30 17:49:48 -0400

    4 4s

---
+++ four_4s.py
@@ -0,0 +1,9 @@
+
+#program to solve depth in 4 4s problem
+# problem desc: use exactly four 4s, theoretically every dgit can be created
+# because the Paul Bourke solution exists and makes every n > 0 trivial, log is a banned function
+# permitted functions: addition, subtraction, div, multiplication, concatentation, sqrt, decimal point (.4), factorial, exponentiation
+
+
+if __name__ == '__main__':
+    
