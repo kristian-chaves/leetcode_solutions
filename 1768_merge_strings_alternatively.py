d9b29c4d1e1e3194250997964372c3c268ac6f63
kristian:) <kristianchaves42@gmail.com>
2024-07-24 17:09:42 -0400

    1768 completed

--- 1768_merge_strings_alternatively.py
+++ 1768_merge_strings_alternatively.py
@@ -10,14 +10,19 @@ def merge_string(s1, s2):
             s3.append(s2[index])
             index += 1
         turn+=1
-    if(turn % 2 == 1):
+    while(len(s1) > index):
+        s3.append(s1[index])
         index+=1
-    if(index > s1):
-        s3.append(s2)
-    else:
-        s3.append(s2)
+    while(len(s2) > index):
+        s3.append(s2[index])
+        index+=1
+    s3 = "".join(s3)
     return s3
 
-s1 = "abc"
-s2 = "pqr"
+# s1 = "abc"
+# s2 = "pqr"
+# s1 = "abcd"
+# s2 = "pq"
+s1 = "ab"
+s2 = "spqr"
 print(merge_string(s1, s2))
