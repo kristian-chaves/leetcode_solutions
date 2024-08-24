21b8b4dc4204e1ab60f7729a25466de1f3bd2067
kristian:) <kristianchaves42@gmail.com>
2024-08-23 20:21:32 -0400

    Update 643_maximum_average_subarray_one.py

--- 643_maximum_average_subarray_one.py
+++ 643_maximum_average_subarray_one.py
@@ -1,14 +1,13 @@
 def findMaxAverage(nums, k):
-    greatest = float('-inf')
-    window = nums[0:k]
-    lp = 0
-    rp = len(nums) - 1
-    for x in range(k, len(nums)+1):
-        greatest = max(sum(nums[(x-k):x])/k, greatest)
+    window = sum(nums[:k])
+    greatest = window/k    
+    for x in range(k, len(nums)):
+        window += nums[x] - nums[x-k]
+        greatest = max(greatest, window/k)
     return greatest
 
 
-nums = [1,12,-5,-6,50,3]
-k = 4
+nums = [5]
+k = 1
 
 print(findMaxAverage(nums, k))
