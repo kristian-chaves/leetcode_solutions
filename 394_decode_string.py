28e26a2ce5f6391d40280a4402760ea0be65a832
kristian:) <kristianchaves42@gmail.com>
2024-09-01 14:58:10 -0400

    Create 394_decode_string.py

---
+++ 394_decode_string.py
@@ -0,0 +1,27 @@
+
+def decodeString(s):
+    final = []
+
+    for x in range(len(s)):
+        if(s[x] != "]"):
+            final.append(s[x])
+        else:
+            q = ""
+            while final[-1] != "[":
+                q += final.pop()
+            q = q[::-1]
+            final.pop()
+
+            num = ""
+            while final and final[-1].isdigit():
+                num += final.pop()
+            num = num[::-1]
+
+            final.append(int(num) * q)            
+    return "".join(final)
+
+
+s = "3[a]2[bc]"
+
+print(decodeString(s))
+
