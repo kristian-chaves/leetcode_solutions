8f7272469c359ed1d9672d87b06f732b7d6a4d25
kristian:) <kristianchaves42@gmail.com>
2024-09-29 14:34:52 -0400

    Update 2215_find_the_difference_of_two_arrays.py

--- 2215_find_the_difference_of_two_arrays.py
+++ 2215_find_the_difference_of_two_arrays.py
@@ -1,13 +1,17 @@
- def isAnagram(s: str, t: str):
-    q = list(s)
-    q.sort()
-    w = list(t)
-    if(len(q) != len(w)):
-        return False
-    w.sort()
-    for x in range(len(w)):
-        if(w[x] != q[x]):
-            return False
-    return True
+def findDiff(nums1, nums2):
+    nums1 = dict.fromkeys(nums1)
+    nums2 = dict.fromkeys(nums2)
+    removeList = []
+    for x in nums1:
+        if(x in nums2):
+            nums2.pop(x)
+            removeList.append(x)
+    for x in removeList:
+        nums1.pop(x)
+    nums1 = list(nums1)
+    nums2 = list(nums2)
+    return [nums1, nums2]
 
-print(isAnagram(s = "rat", t = "tar"))
+nums1 = [1,2,3]
+nums2 = [2,4,6]
+print(findDiff(nums1, nums2))
