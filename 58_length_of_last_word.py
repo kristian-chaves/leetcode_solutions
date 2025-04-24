00678b172592cc76040cb55cd8b16ed4c7105af3
kristian:) <68247804+kristian-chaves@users.noreply.github.com>
2025-04-23 20:37:49 -0400

    Create 58_length_of_last_word.py

---
+++ 58_length_of_last_word.py
@@ -0,0 +1,18 @@
+
+def lengthOfLastWord(s):
+    second = False
+    count = 0
+    for x in reversed(s):
+        if x == ' ':
+            if second == True:
+                return count
+            pass
+        else:
+            count +=1
+            second = True       
+    return count     
+
+word = 'Hello World'
+print(lengthOfLastWord(word))
+
+
