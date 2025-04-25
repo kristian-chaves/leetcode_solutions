71172f132a7cba94ca3423a64d35852dacd7aacf
kristian:) <68247804+kristian-chaves@users.noreply.github.com>
2025-04-24 23:38:03 -0400

    Update 80_remove_duplicates_from_sorted_array_II.py

--- 80_remove_duplicates_from_sorted_array_II.py
+++ 80_remove_duplicates_from_sorted_array_II.py
@@ -1,12 +1,11 @@
 
 def removeDupe2(nums):
-    if(len(nums) > 2):
-        x = 2
-        while x < len(nums): 
-            if(nums[x-2] == nums[x-1] == nums[x]):
-                nums.pop(x)
-                x -= 1
-            x+=1
+    x = 2
+    while x < len(nums): 
+        if(nums[x-2] == nums[x-1] == nums[x]):
+            nums.pop(x)
+            x -= 1
+        x+=1
     return len(nums)
 
 nums = [0,0,1,1,1,1,2,3,3]
