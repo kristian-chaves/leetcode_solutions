d16763720b2c2a6b9d5ccd9c57df4bd0e690e3bf
kristian:) <68247804+kristian-chaves@users.noreply.github.com>
2025-05-14 22:51:50 -0400

    Update 45_jump_game_2.py

--- 45_jump_game_2.py
+++ 45_jump_game_2.py
@@ -7,6 +7,8 @@ def jumpGame2(nums):
     while True:
         if(curIndex >= len(nums) - 1):
             return count
+        if(nums[curIndex] >= len(nums) - curIndex - 1):
+            return count+1
         subarray = nums[curIndex+1: nums[curIndex]+curIndex+1]
         #choose rightmost max index
         for index in reversed(range(len(subarray))):
@@ -15,6 +17,7 @@ def jumpGame2(nums):
                 count+=1
                 break
 
-nums = [1,2,1,1,1]
+#nums = [10,9,8,7,6,5,4,3,2,1,1,0]
+nums = [2,3,1,1,4]
 print(jumpGame2(nums))
 
