88d4d00c06a480dc76ac82d5f89b326a8ae77a81
kristian:) <68247804+kristian-chaves@users.noreply.github.com>
2025-11-13 23:51:28 -0500

    dasfgs

--- another_go_neetcode/longestConsecutive.py
+++ another_go_neetcode/longestConsecutive.py
@@ -1,32 +1,21 @@
 def function(nums):
     longestCount = 0
-    currentCount = 0
     entriesDict = {}
     visitedSet = set()
-    #add to entries dict, then iterate - attain maximum -> work down -- if chain with it exists, skip
+
     for x in nums:
         entriesDict[x+1] = x
     
     for x in nums:
         if x not in visitedSet:
-            # chain to the top
-            chainActive = True
-            while chainActive:
-                if(x+1 in entriesDict):
-                    x+=1
-                else:
-                    chainActive = False    
-            #start chaining down
-            chainActive = True
-            while chainActive:
-                if x in entriesDict:
-                    currentCount += 1
-                    x = entriesDict[x]
-                    visitedSet.add(x)
-                else:
-                    chainActive = False
-            longestCount = max(longestCount,currentCount)
             currentCount = 0
+            while(x+1 in entriesDict):
+                x+=1
+            while x in entriesDict:
+                currentCount += 1
+                x = entriesDict[x]
+                visitedSet.add(x)
+            longestCount = max(longestCount,currentCount)
     return longestCount
 
 
