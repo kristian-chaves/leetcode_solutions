1d1715c453da26d2b94291a91ced26e8a75e6460
kristian:) <68247804+kristian-chaves@users.noreply.github.com>
2025-11-23 15:32:17 -0500

    dasf

---
+++ another_go_neetcode/carFleet.py
@@ -0,0 +1,22 @@
+def function(target, position, speed):
+    fleetSet = {}
+    fleetStack = []
+    #follows slope formula so trying this as base test
+    for x in range(len(speed)):
+        fleetStack.append()
+    return 0
+
+
+target = 12
+position = [10,8,0,5,3]
+speed = [2,4,1,1,3]
+print(function(target, position, speed))
+
+asd
+
+"""
+    outcomesSet = set()
+    for x in range(len(position)):
+        outcomesSet.add((target - position[x]) / speed[x])
+    return len(outcomesSet)
+"""
